3ffdc2641f28fd6906f3dee4385e42cf834ffc22
ZionFrost <inaki337@gmail.com>
2023-06-03 13:54:49 +0200

    EndScreen

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+
+    public void Pause(InputAction.CallbackContext context)
+    {
+        if (context.performed )
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }  
+        }
+    }
+
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+}
